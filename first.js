ff6c1560ff347c531fa1d169232fde21e94bf063
YashTiwari.YT <yashtiwari.ty@gmail.com>
2022-11-02 06:13:46 +0000

    Update first.js

--- first.js
+++ first.js
@@ -1,13 +1,15 @@
-function checkpalidrome(string){
-    const len= string.length;
-}
-for(let i=0; i<len/2;i++){
-    if(string[i]!==string[len-1-i]){
-        return 'It is not a palindrome';
+function checkpalidrome(string) {
+  const len = string.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    if (string[i] !== string[len - 1 - i]) {
+      return "It is not a palindrome";
     }
-    return 'It is a palindrome';
+    return "It is a palindrome";
+  }
 }
-const string= prompt('Enter a String:');
-const value= checkpalidrome(string);
+const value = checkpalidrome(process.argv[2]);
 
 console.log(value);
+
+// console.log(process)
